15aaa413e058d763fe884ddd3bd9deb750af06ea
Canming Huang <support@emgu.com>
2024-06-04 08:52:22 -0400

    Added license and readme files.

--- Emgu-CV-TFLite-Maui/MainPage.xaml.cs
+++ Emgu-CV-TFLite-Maui/MainPage.xaml.cs
@@ -4,7 +4,6 @@ namespace EmguMauiApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
@@ -15,7 +14,7 @@ namespace EmguMauiApp
         {
             CounterBtn.Text = String.Format("Emgu TF Lite version: {0}\n\n\nEmgu CV Build Info: {1}", Emgu.TF.Lite.TfLiteInvoke.Version, Emgu.CV.CvInvoke.BuildInformation);
             
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
---
+++ README.md
@@ -0,0 +1,12 @@
+==================================================================
+
+This repository contains Emgu CV & Emgu TF demo projects. 
+
+
+Emgu-CV-TFLite-Maui: 
+This folder contains a demo project that use both Emgu CV and Emgu TF Lite in MAUI. This project requires using both Emgu CV and Emgu TF Lite commercial nuget repository.
+
+Please visit our project webpage for more information:
+http://www.emgu.com/wiki/index.php/Main_Page
+
+
